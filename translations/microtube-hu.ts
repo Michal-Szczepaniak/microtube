3664a2f15b5eff8cb40ef3573d7b35c8367f5802
1Zgp <36508793+1Zgp@users.noreply.github.com>
2019-09-02 20:31:47 +0200

    Add files via upload

---
+++ translations/microtube-hu.ts
@@ -0,0 +1,469 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1">
+<context>
+    <name>About</name>
+    <message>
+        <source>About</source>
+        <translation>Mévjegy</translation>
+    </message>
+    <message>
+        <source>Made by Michał Szczepaniak. Based on minitube.</source>
+        <translation>Készítette Michał Szczepaniak. A minitube-on alapul.</translation>
+    </message>
+    <message>
+        <source>Licensed under GPLv3</source>
+        <translation>Elérhető GPLv3 hatálya alatt</translation>
+    </message>
+    <message>
+        <source>Links</source>
+        <translation>Hivatkozások</translation>
+    </message>
+    <message>
+        <source>Thanks to</source>
+        <translation>Köszönet</translation>
+    </message>
+    <message>
+        <source>Donate</source>
+        <translation>Adományozz</translation>
+    </message>
+    <message>
+        <source>jgibbon for german translation and help.</source>
+        <translation>jgibbon-nak a német fordításért és a segítségért,</translation>
+    </message>
+    <message>
+        <source>carmenfdezb for spanish translation.</source>
+        <translation>carmenfdezb-nek a spanyol fordításért,</translation>
+    </message>
+    <message>
+        <source>DylanVanAssche for help.</source>
+        <translation>DylanVanAssche-nak a segítségért.</translation>
+    </message>
+</context>
+<context>
+    <name>ChannelAggregator</name>
+    <message>
+        <source>By %1</source>
+        <translation>%1 által</translation>
+    </message>
+    <message numerus="yes">
+        <source>You have %n new video(s)</source>
+        <translation>%n új videód van</translation>
+    </message>
+</context>
+<context>
+    <name>CoverPage</name>
+    <message>
+        <source>µtube</source>
+        <translation>µtube</translation>
+    </message>
+    <message>
+        <source>Playing:</source>
+        <translation>Fut:</translation>
+    </message>
+</context>
+<context>
+    <name>DataUtils</name>
+    <message>
+        <source>Just now</source>
+        <translation>Épp most</translation>
+    </message>
+    <message numerus="yes">
+        <source>%n hour(s) ago</source>
+        <translation>%n órával ezelőtt</translation>
+    </message>
+    <message numerus="yes">
+        <source>%n day(s) ago</source>
+        <translation>%n nappal ezelőtt</translation>
+    </message>
+    <message numerus="yes">
+        <source>%n month(s) ago</source>
+        <translation>%n hónappal ezelőtt</translation>
+    </message>
+    <message numerus="yes">
+        <source>%n week(s) ago</source>
+        <translation>%n héttel ezelőtt</translation>
+    </message>
+    <message>
+        <source>K</source>
+        <comment>K as in Kilo, i.e. thousands</comment>
+        <translation>Ezer</translation>
+    </message>
+    <message>
+        <source>M</source>
+        <comment>M stands for Millions</comment>
+        <translation>Millió</translation>
+    </message>
+    <message>
+        <source>B</source>
+        <comment>B stands for Billions</comment>
+        <translation>Milliárd</translation>
+    </message>
+    <message>
+        <source>%1 views</source>
+        <translation>%1 megtekintés</translation>
+    </message>
+</context>
+<context>
+    <name>Main</name>
+    <message>
+        <source>Subscriptions</source>
+        <translation>Feliratkozások</translation>
+    </message>
+    <message>
+        <source>Search</source>
+        <translation>Keresés</translation>
+    </message>
+    <message>
+        <source>About</source>
+        <translation>Névjegy</translation>
+    </message>
+    <message>
+        <source>Settings</source>
+        <translation>Beállítások</translation>
+    </message>
+</context>
+<context>
+    <name>PlaylistModel</name>
+    <message>
+        <source>Show %1 More</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>No videos</source>
+        <translation>Nincs videó</translation>
+    </message>
+    <message>
+        <source>No more videos</source>
+        <translation>Nincs több videó</translation>
+    </message>
+    <message>
+        <source>Check internet connection.</source>
+        <translation>Ellenőrizd az interetkapcsolatot.</translation>
+    </message>
+</context>
+<context>
+    <name>Settings</name>
+    <message>
+        <source>Settings</source>
+        <translation>Beállítások</translation>
+    </message>
+    <message>
+        <source>Restricted mode (safe for kids)</source>
+        <translation>Tiltott mód (gyerekeknek biztonságos)</translation>
+    </message>
+    <message>
+        <source>Automatic change to next video</source>
+        <translation>Automatikus váltás a következő videóra</translation>
+    </message>
+    <message>
+        <source>Search for related videos</source>
+        <translation>Kapcsolódó videók keresése</translation>
+    </message>
+</context>
+<context>
+    <name>SharePage</name>
+    <message>
+        <source>Share</source>
+        <translation>Megosztás</translation>
+    </message>
+</context>
+<context>
+    <name>Subscriptions</name>
+    <message>
+        <source>Subscriptions</source>
+        <translation>Feliratkozások</translation>
+    </message>
+    <message>
+        <source>Unsubscribe</source>
+        <translation>Leiratkozás</translation>
+    </message>
+    <message>
+        <source>Mark all as watched</source>
+        <translation>Az összes megjelölése megnézettként</translation>
+    </message>
+</context>
+<context>
+    <name>VideoElement</name>
+    <message>
+        <source>Unsubscribe</source>
+        <translation>Leiratkozás</translation>
+    </message>
+    <message>
+        <source>Subscribe</source>
+        <translation>Feliratkozás</translation>
+    </message>
+    <message>
+        <source>Copy url</source>
+        <translation>Url másolása</translation>
+    </message>
+</context>
+<context>
+    <name>VideoPlayer</name>
+    <message>
+        <source>Downloaded to</source>
+        <translation>Letöltés ide</translation>
+    </message>
+    <message>
+        <source>720p</source>
+        <translation>720p</translation>
+    </message>
+    <message>
+        <source>480p</source>
+        <translation>480p</translation>
+    </message>
+    <message>
+        <source>360p</source>
+        <translation>360p</translation>
+    </message>
+    <message>
+        <source>Download</source>
+        <translation>Letöltés</translation>
+    </message>
+    <message>
+        <source>Copy url</source>
+        <translation>Url másolása</translation>
+    </message>
+    <message>
+        <source>Error: Problem with allocating resources</source>
+        <translation>Hiba: Probléma az erőforrások hozzárendelésében</translation>
+    </message>
+    <message>
+        <source>Error: Media service error</source>
+        <translation>Hiba: Médiaszolgáltatási hiba</translation>
+    </message>
+    <message>
+        <source>Error: Video or Audio format is not supported</source>
+        <translation>Hiba: A videó- vagy audióformátum nem támogatott</translation>
+    </message>
+    <message>
+        <source>Error: Access denied to the video</source>
+        <translation>Hiba: A videóhoz való hozzáférés megtagadva</translation>
+    </message>
+    <message>
+        <source>Error: Network error</source>
+        <translation>Hiba: Hálózati hiba</translation>
+    </message>
+    <message>
+        <source>Unsubscribe</source>
+        <translation>Leiratkozás</translation>
+    </message>
+    <message>
+        <source>Subscribe</source>
+        <translation>Feliratkozás</translation>
+    </message>
+</context>
+<context>
+    <name>YT</name>
+    <message>
+        <source>All Videos</source>
+        <translation>Minden videó</translation>
+    </message>
+    <message>
+        <source>Unwatched Videos</source>
+        <translation>Megnézetlen videók</translation>
+    </message>
+</context>
+<context>
+    <name>YTRegions</name>
+    <message>
+        <source>Algeria</source>
+        <translation>Algéria</translation>
+    </message>
+    <message>
+        <source>Argentina</source>
+        <translation>Argentína</translation>
+    </message>
+    <message>
+        <source>Australia</source>
+        <translation>Ausztrália</translation>
+    </message>
+    <message>
+        <source>Belgium</source>
+        <translation>Belgium</translation>
+    </message>
+    <message>
+        <source>Brazil</source>
+        <translation>Brazília</translation>
+    </message>
+    <message>
+        <source>Canada</source>
+        <translation>Kanada</translation>
+    </message>
+    <message>
+        <source>Chile</source>
+        <translation>Chile</translation>
+    </message>
+    <message>
+        <source>Colombia</source>
+        <translation>Kolumbia</translation>
+    </message>
+    <message>
+        <source>Czech Republic</source>
+        <translation>Cseh Köztársaság</translation>
+    </message>
+    <message>
+        <source>Egypt</source>
+        <translation>Egyiptom</translation>
+    </message>
+    <message>
+        <source>France</source>
+        <translation>Franciaország</translation>
+    </message>
+    <message>
+        <source>Germany</source>
+        <translation>Németország</translation>
+    </message>
+    <message>
+        <source>Ghana</source>
+        <translation>Ghána</translation>
+    </message>
+    <message>
+        <source>Greece</source>
+        <translation>Görögország</translation>
+    </message>
+    <message>
+        <source>Hong Kong</source>
+        <translation>Hong Kong</translation>
+    </message>
+    <message>
+        <source>Hungary</source>
+        <translation>Magyarország</translation>
+    </message>
+    <message>
+        <source>India</source>
+        <translation>India</translation>
+    </message>
+    <message>
+        <source>Indonesia</source>
+        <translation>Indonézia</translation>
+    </message>
+    <message>
+        <source>Ireland</source>
+        <translation>Írország</translation>
+    </message>
+    <message>
+        <source>Israel</source>
+        <translation>Izrael</translation>
+    </message>
+    <message>
+        <source>Italy</source>
+        <translation>Olaszország</translation>
+    </message>
+    <message>
+        <source>Japan</source>
+        <translation>Japán</translation>
+    </message>
+    <message>
+        <source>Jordan</source>
+        <translation>Jordánia</translation>
+    </message>
+    <message>
+        <source>Kenya</source>
+        <translation>Kenya</translation>
+    </message>
+    <message>
+        <source>Malaysia</source>
+        <translation>Malajzia</translation>
+    </message>
+    <message>
+        <source>Mexico</source>
+        <translation>Mexikó</translation>
+    </message>
+    <message>
+        <source>Morocco</source>
+        <translation>Marokkó</translation>
+    </message>
+    <message>
+        <source>Netherlands</source>
+        <translation>Hollandia</translation>
+    </message>
+    <message>
+        <source>New Zealand</source>
+        <translation>Új-Zéland</translation>
+    </message>
+    <message>
+        <source>Nigeria</source>
+        <translation>Nigéria</translation>
+    </message>
+    <message>
+        <source>Peru</source>
+        <translation>Peru</translation>
+    </message>
+    <message>
+        <source>Philippines</source>
+        <translation>Fülöp-szigetek</translation>
+    </message>
+    <message>
+        <source>Poland</source>
+        <translation>Lengyelország</translation>
+    </message>
+    <message>
+        <source>Russia</source>
+        <translation>Oroszország</translation>
+    </message>
+    <message>
+        <source>Saudi Arabia</source>
+        <translation>Szaúd-Arábia</translation>
+    </message>
+    <message>
+        <source>Singapore</source>
+        <translation>Szingapúr</translation>
+    </message>
+    <message>
+        <source>South Africa</source>
+        <translation>Dél-Afrika</translation>
+    </message>
+    <message>
+        <source>South Korea</source>
+        <translation>Dél-Korea</translation>
+    </message>
+    <message>
+        <source>Spain</source>
+        <translation>Spanyolország</translation>
+    </message>
+    <message>
+        <source>Sweden</source>
+        <translation>Svédország</translation>
+    </message>
+    <message>
+        <source>Taiwan</source>
+        <translation>Taiwan</translation>
+    </message>
+    <message>
+        <source>Tunisia</source>
+        <translation>Tunézia</translation>
+    </message>
+    <message>
+        <source>Turkey</source>
+        <translation>Törökország</translation>
+    </message>
+    <message>
+        <source>Uganda</source>
+        <translation>Uganda</translation>
+    </message>
+    <message>
+        <source>United Arab Emirates</source>
+        <translation>Egyesült Arab Emirátusok</translation>
+    </message>
+    <message>
+        <source>United Kingdom</source>
+        <translation>Egyesült Királyság</translation>
+    </message>
+    <message>
+        <source>Yemen</source>
+        <translation>Jemen</translation>
+    </message>
+    <message>
+        <source>Worldwide</source>
+        <translation>Világszerte</translation>
+    </message>
+</context>
+<context>
+    <name>YTVideo</name>
+    <message>
+        <source>Cannot get video stream for %1</source>
+        <translation>Nem található videostream erre: %1</translation>
+    </message>
+</context>
+</TS>
