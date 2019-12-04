39caab0e11e72d7b5f710865346109e48cffb2ad
dashinfantry <45334556+dashinfantry@users.noreply.github.com>
2019-12-04 18:11:02 +0800

    Create harbour-microtube-zh_CN.ts

---
+++ translations/harbour-microtube-zh_CN.ts
@@ -0,0 +1,491 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1">
+<context>
+    <name>About</name>
+    <message>
+        <source>About</source>
+        <translation>关于</translation>
+    </message>
+    <message>
+        <source>Made by Michał Szczepaniak. Based on minitube.</source>
+        <translation>由 Michał Szczepaniak 开发，基于 minitube 。</translation>
+    </message>
+    <message>
+        <source>Licensed under GPLv3</source>
+        <translation>采用 GPLv3 许可协议</translation>
+    </message>
+    <message>
+        <source>Links</source>
+        <translation>链接</translation>
+    </message>
+    <message>
+        <source>Thanks to</source>
+        <translation>感谢</translation>
+    </message>
+    <message>
+        <source>Donate</source>
+        <translation>捐赠</translation>
+    </message>
+    <message>
+        <source>jgibbon for german translation and help.</source>
+        <translation>jgibbon 提供德语翻译及开发协助。</translation>
+    </message>
+    <message>
+        <source>carmenfdezb for spanish translation.</source>
+        <translation>carmenfdezb 提供西班牙语翻译</translation>
+    </message>
+    <message>
+        <source>DylanVanAssche for help.</source>
+        <translation>DylanVanAssche 开发协助。</translation>
+    </message>
+    <message>
+        <source>1Zgp for hungarian translation.</source>
+        <translation>1Zgp 提供匈牙利语翻译。</translation>
+    </message>
+</context>
+<context>
+    <name>ChannelAggregator</name>
+    <message>
+        <source>By %1</source>
+        <translation>视频来自 %1</translation>
+    </message>
+    <message numerus="yes">
+        <source>You have %n new video(s)</source>
+        <translation>
+            <numerusform>你有 %1 新视频</numerusform>
+        </translation>
+    </message>
+</context>
+<context>
+    <name>CoverPage</name>
+    <message>
+        <source>µtube</source>
+        <translation>µtube</translation>
+    </message>
+    <message>
+        <source>Playing:</source>
+        <translation>正在播放:</translation>
+    </message>
+</context>
+<context>
+    <name>DataUtils</name>
+    <message>
+        <source>Just now</source>
+        <translation>刚刚</translation>
+    </message>
+    <message numerus="yes">
+        <source>%n hour(s) ago</source>
+        <translation>
+            <numerusform>%n 小时前</numerusform>
+        </translation>
+    </message>
+    <message numerus="yes">
+        <source>%n day(s) ago</source>
+        <translation>
+            <numerusform>%n 天前</numerusform>
+        </translation>
+    </message>
+    <message numerus="yes">
+        <source>%n month(s) ago</source>
+        <translation>
+            <numerusform>%n 月前</numerusform>
+        </translation>
+    </message>
+    <message numerus="yes">
+        <source>%n week(s) ago</source>
+        <translation>
+            <numerusform>%n 周前</numerusform>
+        </translation>
+    </message>
+    <message>
+        <source>K</source>
+        <comment>K as in Kilo, i.e. thousands</comment>
+        <translation>K</translation>
+    </message>
+    <message>
+        <source>M</source>
+        <comment>M stands for Millions</comment>
+        <translation>M</translation>
+    </message>
+    <message>
+        <source>B</source>
+        <comment>B stands for Billions</comment>
+        <translation>B</translation>
+    </message>
+    <message>
+        <source>%1 views</source>
+        <translation>%1 次观看</translation>
+    </message>
+</context>
+<context>
+    <name>Main</name>
+    <message>
+        <source>Subscriptions</source>
+        <translation>订阅</translation>
+    </message>
+    <message>
+        <source>Search</source>
+        <translation>搜索</translation>
+    </message>
+    <message>
+        <source>About</source>
+        <translation>关于</translation>
+    </message>
+    <message>
+        <source>Settings</source>
+        <translation>设置</translation>
+    </message>
+</context>
+<context>
+    <name>PlaylistModel</name>
+    <message>
+        <source>Show %1 More</source>
+        <translation>多显示 %1 个视频</translation>
+    </message>
+    <message>
+        <source>No videos</source>
+        <translation>无视频</translation>
+    </message>
+    <message>
+        <source>No more videos</source>
+        <translation>没有更多视频</translation>
+    </message>
+    <message>
+        <source>Check internet connection.</source>
+        <translation>检查网络连接。</translation>
+    </message>
+</context>
+<context>
+    <name>Settings</name>
+    <message>
+        <source>Settings</source>
+        <translation>设置</translation>
+    </message>
+    <message>
+        <source>Restricted mode (safe for kids)</source>
+        <translation>严格模式（对儿童安全）</translation>
+    </message>
+    <message>
+        <source>Automatic change to next video</source>
+        <translation>自动播放下一个视频</translation>
+    </message>
+    <message>
+        <source>Search for related videos</source>
+        <translation>搜索相关视频</translation>
+    </message>
+    <message>
+        <source>Region</source>
+        <translation>区域</translation>
+    </message>
+    <message>
+        <source>Download location</source>
+        <translation>下载位置</translation>
+    </message>
+</context>
+<context>
+    <name>SharePage</name>
+    <message>
+        <source>Share</source>
+        <translation>分享</translation>
+    </message>
+</context>
+<context>
+    <name>Subscriptions</name>
+    <message>
+        <source>Subscriptions</source>
+        <translation>订阅</translation>
+    </message>
+    <message>
+        <source>Unsubscribe</source>
+        <translation>取消订阅</translation>
+    </message>
+    <message>
+        <source>Mark all as watched</source>
+        <translation>标记为已看</translation>
+    </message>
+</context>
+<context>
+    <name>VideoElement</name>
+    <message>
+        <source>Unsubscribe</source>
+        <translation>取消订阅</translation>
+    </message>
+    <message>
+        <source>Subscribe</source>
+        <translation>订阅</translation>
+    </message>
+    <message>
+        <source>Copy url</source>
+        <translation>复制 url</translation>
+    </message>
+</context>
+<context>
+    <name>VideoPlayer</name>
+    <message>
+        <source>Downloaded to</source>
+        <translation>下载到</translation>
+    </message>
+    <message>
+        <source>720p</source>
+        <translation>720p</translation>
+    </message>
+    <message>
+        <source>360p</source>
+        <translation>360p</translation>
+    </message>
+    <message>
+        <source>Download</source>
+        <translation>下载</translation>
+    </message>
+    <message>
+        <source>Copy url</source>
+        <translation>复制 url</translation>
+    </message>
+    <message>
+        <source>Error: Problem with allocating resources</source>
+        <translation>错误:分配资源出错</translation>
+    </message>
+    <message>
+        <source>Error: Media service error</source>
+        <translation>错误:媒体服务错误</translation>
+    </message>
+    <message>
+        <source>Error: Video or Audio format is not supported</source>
+        <translation>错误:不支持的视频或音频格式</translation>
+    </message>
+    <message>
+        <source>Error: Access denied to the video</source>
+        <translation>错误:访问视频被拒绝</translation>
+    </message>
+    <message>
+        <source>Error: Network error</source>
+        <translation>错误:网络错误</translation>
+    </message>
+    <message>
+        <source>Unsubscribe</source>
+        <translation>取消订阅</translation>
+    </message>
+    <message>
+        <source>Subscribe</source>
+        <translation>订阅</translation>
+    </message>
+</context>
+<context>
+    <name>YT</name>
+    <message>
+        <source>All Videos</source>
+        <translation>全部视频</translation>
+    </message>
+    <message>
+        <source>Unwatched Videos</source>
+        <translation>未看视频</translation>
+    </message>
+</context>
+<context>
+    <name>YTRegions</name>
+    <message>
+        <source>Algeria</source>
+        <translation>阿尔及利亚</translation>
+    </message>
+    <message>
+        <source>Argentina</source>
+        <translation>阿根廷</translation>
+    </message>
+    <message>
+        <source>Australia</source>
+        <translation>澳大利亚</translation>
+    </message>
+    <message>
+        <source>Belgium</source>
+        <translation>比利时</translation>
+    </message>
+    <message>
+        <source>Brazil</source>
+        <translation>巴西</translation>
+    </message>
+    <message>
+        <source>Canada</source>
+        <translation>加拿大</translation>
+    </message>
+    <message>
+        <source>Chile</source>
+        <translation>智利</translation>
+    </message>
+    <message>
+        <source>Colombia</source>
+        <translation>哥伦比亚</translation>
+    </message>
+    <message>
+        <source>Czech Republic</source>
+        <translation>捷克共和国</translation>
+    </message>
+    <message>
+        <source>Egypt</source>
+        <translation>埃及</translation>
+    </message>
+    <message>
+        <source>France</source>
+        <translation>法国</translation>
+    </message>
+    <message>
+        <source>Germany</source>
+        <translation><德国/translation>
+    </message>
+    <message>
+        <source>Ghana</source>
+        <translation>加纳</translation>
+    </message>
+    <message>
+        <source>Greece</source>
+        <translation>希腊</translation>
+    </message>
+    <message>
+        <source>Hong Kong</source>
+        <translation>中国香港</translation>
+    </message>
+    <message>
+        <source>Hungary</source>
+        <translation>匈牙利</translation>
+    </message>
+    <message>
+        <source>India</source>
+        <translation>印度</translation>
+    </message>
+    <message>
+        <source>Indonesia</source>
+        <translation>印度尼西亚</translation>
+    </message>
+    <message>
+        <source>Ireland</source>
+        <translation>爱尔兰</translation>
+    </message>
+    <message>
+        <source>Israel</source>
+        <translation>以色列</translation>
+    </message>
+    <message>
+        <source>Italy</source>
+        <translation>意大利</translation>
+    </message>
+    <message>
+        <source>Japan</source>
+        <translation>日本</translation>
+    </message>
+    <message>
+        <source>Jordan</source>
+        <translation>约旦</translation>
+    </message>
+    <message>
+        <source>Kenya</source>
+        <translation>肯尼亚</translation>
+    </message>
+    <message>
+        <source>Malaysia</source>
+        <translation>马来西亚</translation>
+    </message>
+    <message>
+        <source>Mexico</source>
+        <translation>墨西哥</translation>
+    </message>
+    <message>
+        <source>Morocco</source>
+        <translation>摩洛哥</translation>
+    </message>
+    <message>
+        <source>Netherlands</source>
+        <translation>荷兰</translation>
+    </message>
+    <message>
+        <source>New Zealand</source>
+        <translation>新西兰</translation>
+    </message>
+    <message>
+        <source>Nigeria</source>
+        <translation>尼日利亚</translation>
+    </message>
+    <message>
+        <source>Peru</source>
+        <translation>秘鲁</translation>
+    </message>
+    <message>
+        <source>Philippines</source>
+        <translation>菲律宾</translation>
+    </message>
+    <message>
+        <source>Poland</source>
+        <translation>波兰</translation>
+    </message>
+    <message>
+        <source>Russia</source>
+        <translation>俄罗斯</translation>
+    </message>
+    <message>
+        <source>Saudi Arabia</source>
+        <translation>沙特阿拉伯</translation>
+    </message>
+    <message>
+        <source>Singapore</source>
+        <translation>新加坡</translation>
+    </message>
+    <message>
+        <source>South Africa</source>
+        <translation>南非</translation>
+    </message>
+    <message>
+        <source>South Korea</source>
+        <translation>韩国</translation>
+    </message>
+    <message>
+        <source>Spain</source>
+        <translation>西班牙</translation>
+    </message>
+    <message>
+        <source>Sweden</source>
+        <translation>瑞典</translation>
+    </message>
+    <message>
+        <source>Taiwan</source>
+        <translation>中国台湾</translation>
+    </message>
+    <message>
+        <source>Tunisia</source>
+        <translation>突尼斯</translation>
+    </message>
+    <message>
+        <source>Turkey</source>
+        <translation>土耳其</translation>
+    </message>
+    <message>
+        <source>Uganda</source>
+        <translation>乌干达</translation>
+    </message>
+    <message>
+        <source>United Arab Emirates</source>
+        <translation>阿拉伯联合酋长国</translation>
+    </message>
+    <message>
+        <source>United Kingdom</source>
+        <translation>英国</translation>
+    </message>
+    <message>
+        <source>Yemen</source>
+        <translation>也门</translation>
+    </message>
+    <message>
+        <source>Worldwide</source>
+        <translation>全球</translation>
+    </message>
+    <message>
+        <source>United States</source>
+        <translation>美国</translation>
+    </message>
+</context>
+<context>
+    <name>YTVideo</name>
+    <message>
+        <source>Cannot get video stream for %1</source>
+        <translation>无法获取 %1 视频流媒体</translation>
+    </message>
+</context>
+</TS>
